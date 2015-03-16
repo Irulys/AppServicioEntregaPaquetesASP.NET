850ebaf4540ba5d1a9885b02bac981c8af3ca8d4
eapuerta <eapuerta@gmail.com>
2015-03-16 17:00:01 -0400

    bills

---
+++ App/Modelo/Bills.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class Bills
+    {
+        private long id;
+        private DateTime date;
+        private Person customers;
+        protected Dictionary<Package, double> items = new Dictionary<Package, double>();
+        protected long val = 0;
+
+
+        public long Id
+        {
+            get { return id; }
+            set { id = val++; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public Person Customers
+        {
+            get { return customers; }
+            set { customers = value; }
+        }
+
+
+    }
+}
