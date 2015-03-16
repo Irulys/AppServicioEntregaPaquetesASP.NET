3d98beef9ba848726bde66554a27910d0169a6bc
eapuerta <eapuerta@gmail.com>
2015-03-16 17:25:17 -0400

    billsUpdate

--- App/Modelo/Bills.cs
+++ App/Modelo/Bills.cs
@@ -11,13 +11,13 @@ namespace App.Modelo
         private DateTime date;
         private Person customers;
         protected Dictionary<Package, double> items = new Dictionary<Package, double>();
-        protected long val = 0;
+        protected static long val = 0;
 
 
         public long Id
         {
             get { return id; }
-            set { id = val++; }
+            set { id = ++val; }
         }
         public DateTime Date
         {
@@ -30,6 +30,47 @@ namespace App.Modelo
             set { customers = value; }
         }
 
+        public Bills()
+        {
+            this.id = ++val;
+            this.date = new DateTime(short);
+            this.items  = null;
+        }
+        public Bills(DateTime date, Dictionary<Package, double> packages)
+        {
+            this.id = ++val;
+            this.date = date;
+            this.items = packages;
+        }
+
+
+        #region "Métodos SobreEscritos"
+        public override string ToString()
+        {
+            return "\nId :" + this.id +
+                   "\nDate :" + this.date +
+                   "\nCustomer :" + this.customers +
+                   "\n_______________________________________";
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        /*
+        public override bool Equals(object obj)
+        {
+            TwoDayPackage o = (TwoDayPackage)obj;
+            bool result = false;
+
+            if (base.Equals(o) && (this.fixedFee == o.fixedFee))
+
+                result = true;
+
+            return result;
+        }
+         * */
+
+        #endregion
 
     }
 }
